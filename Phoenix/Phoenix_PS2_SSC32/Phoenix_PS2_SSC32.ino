8d3f4379248468f406129a6c2d39d97fa3816066
Kurt Eckhardt <kurte@rockisland.com>
2014-01-13 13:16:04 -0800

    Add Phoenix Example Configuration

---
+++ Phoenix/Phoenix_PS2_SSC32/Phoenix_PS2_SSC32.ino
@@ -0,0 +1,36 @@
+//=============================================================================
+//Project Lynxmotion Phoenix
+//Description: Phoenix software
+//Software version: V2.0
+//Date: 29-10-2009
+//Programmer: Jeroen Janssen [aka Xan]
+//         Kurt Eckhardt(KurtE) converted to C and Arduino
+//   Kåre Halvorsen aka Zenta - Makes everything work correctly!     
+//
+// This version of the Phoenix code was ported over to the Arduino Environement
+// and is specifically configured for the Lynxmotion BotBoarduino 
+//
+//=============================================================================
+//
+//KNOWN BUGS:
+//    - Lots ;)
+//
+//=============================================================================
+// Header Files
+//=============================================================================
+
+#define DEFINE_HEX_GLOBALS
+#if ARDUINO>99
+#include <Arduino.h>
+#else
+#endif
+#include <EEPROM.h>
+#include <PS2X_lib.h>
+#include <SoftwareSerial.h>
+
+#include "Hex_CFG.h"
+#include <Phoenix.h>
+#include <Phoenix_Input_PS2.h>
+#include <Phoenix_Driver_SSC32.h>
+#include <Phoenix_Code.h>
+
