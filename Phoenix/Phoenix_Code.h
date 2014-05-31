82d1901a402d67b3ca1faca52aba45d600b22096
Kurt Eckhardt <kurte@rockisland.com>
2014-05-30 18:23:19 -0700

    Compile Error 4dof

--- Phoenix/Phoenix_Code.h
+++ Phoenix/Phoenix_Code.h
@@ -996,7 +996,7 @@ void StartUpdateServos()
         cCoxaInv[LegIndex]? -CoxaAngle1[LegIndex] : CoxaAngle1[LegIndex], 
         cFemurInv[LegIndex]? -FemurAngle1[LegIndex] : FemurAngle1[LegIndex], 
         cTibiaInv[LegIndex]? -TibiaAngle1[LegIndex] : TibiaAngle1[LegIndex], 
-        cTarsInv[LegIndex]? -TarsAngle1[LegIndex]) : TarsAngle1[LegIndex]);
+        cTarsInv[LegIndex]? -TarsAngle1[LegIndex] : TarsAngle1[LegIndex]);
 #else
     g_ServoDriver.OutputServoInfoForLeg(LegIndex, 
         cCoxaInv[LegIndex]? -CoxaAngle1[LegIndex] : CoxaAngle1[LegIndex], 
